b89644d2c94a1d07bdfd8eae4e709b16f52fc024
AvaniDange <dangeavani@gmail.com>
2024-10-03 15:34:42 +0530

    home

--- src/App.jsx
+++ src/App.jsx
@@ -1,18 +1,24 @@
 import React from "react";
 import { Route, Routes } from "react-router-dom";
 import Login from "./pages/Login";
+import Home from "./pages/Home";  // Import the Home component
 import { useAuthState } from "react-firebase-hooks/auth";
-import { auth, db } from "./config/firebase-config";
+import { auth } from "./config/firebase-config"; // 'db' was imported but not used, so it's removed here
 
 function App() {
   const [user] = useAuthState(auth);
+
   return (
     <>
-     <Routes>
-      <Route path='/login' element={<Login/>} />
-     </Routes>
+      <Routes>
+        <Route path='/login' element={<Login />} />
+        {/* Add the Home component route */}
+        <Route path='/home' element={<Home />} />
+        {/* Optionally, redirect root path to Home or Login based on auth state */}
+        <Route path='/' element={user ? <Home /> : <Login />} />
+      </Routes>
     </>
-  )
+  );
 }
 
-export default App
+export default App;
