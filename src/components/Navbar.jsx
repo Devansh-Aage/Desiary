fcc8d83ec6555dbbe5168eeee1f77ad5adb44392
aryagami27 <94152700+aryagami27@users.noreply.github.com>
2024-10-12 13:14:43 +0530

    Home page started to reform

---
+++ src/components/Navbar.jsx
@@ -0,0 +1,11 @@
+import React from 'react'
+
+function Navbar() {
+  return (
+    <div className='w-screen h-16 bg-red-500'>
+      
+    </div>
+  )
+}
+
+export default Navbar
--- src/main.jsx
+++ src/main.jsx
@@ -4,13 +4,15 @@ import './index.css'
 import {createBrowserRouter, RouterProvider, createRoutesFromElements, Route} from 'react-router-dom'
 import Login from './pages/Login.jsx'
 import Signup from './pages/Signup.jsx'
+import Layout from './Layout.jsx'
+import Home1 from './pages/Home.jsx'
 
 const router = createBrowserRouter(
   createRoutesFromElements(
     <>
-      {/* <Route path='/home' element={<Layout1/>}>
-        <Route index element={<Home2/>} />
-      </Route> */}
+      <Route path='/home' element={<Layout/>}>
+        <Route index element={<Home1/>} />
+      </Route>
       <Route path='/' element={<Login/>} />
       <Route path='/signup' element={<Signup/>} />
     </>
