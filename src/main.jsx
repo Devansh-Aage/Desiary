fce58a099bcaa53721b9f43b5f636426e5435953
aryagami27 <94152700+aryagami27@users.noreply.github.com>
2024-10-12 13:05:37 +0530

    Changed whole project layout

--- src/main.jsx
+++ src/main.jsx
@@ -1,13 +1,24 @@
 import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
-import App from './App.jsx'
 import './index.css'
-import { BrowserRouter } from 'react-router-dom'
+import {createBrowserRouter, RouterProvider, createRoutesFromElements, Route} from 'react-router-dom'
+import Login from './pages/Login.jsx'
+import Signup from './pages/Signup.jsx'
+
+const router = createBrowserRouter(
+  createRoutesFromElements(
+    <>
+      {/* <Route path='/home' element={<Layout1/>}>
+        <Route index element={<Home2/>} />
+      </Route> */}
+      <Route path='/' element={<Login/>} />
+      <Route path='/signup' element={<Signup/>} />
+    </>
+  )
+)
 
 createRoot(document.getElementById('root')).render(
   <StrictMode>
-    <BrowserRouter>
-    <App />
-    </BrowserRouter>
+    <RouterProvider router={router} />
   </StrictMode>,
 )
--- src/pages/Signup.jsx
+++ src/pages/Signup.jsx
@@ -1,34 +1,26 @@
-import { signInWithPopup, GoogleAuthProvider } from "firebase/auth";
-import { auth, provider } from "../config/firebase-config";
-import { useState } from "react";
-import { Link } from "react-router-dom";
+import { Link } from "react-router-dom"
 
-const Login = () => {
-  const [userState, setUserState] = useState(null);
-
-  const handleSignIn = async () => {
-    try {
-      const result = await signInWithPopup(auth, provider);
-      const user = result.user;
-      console.log("User signed in:", user);
-      setUserState(user);
-    } catch (error) {
-      console.error("Error:", error);
-    }
-  };
-
+function Signup() {
   return (
     <div className="w-[100vw] h-[100vh] flex justify-center items-center">
     <div className="h-[80vh] w-[80vw] max-w-[1300px] max-h-[900px] flex shadow-2xl rounded-[40px]">
       <div className="w-[60%] h-full bg-gradient-to-tr from-[#F8B59A] to-[#FDE1EF] via-[#F27EA1] via rounded-l-[40px] flex justify-center items-center">
         <div className="flex-col">
-          <p className='text-5xl font-medium text-center tracking-tightiest'>Log-in</p>
-          <p className='text-center font-medium text-lg text-[#3A3A3A]' >Where have you been?</p>
+          <p className='text-5xl font-medium text-center tracking-tightiest'>Sign up</p>
+          <p className='text-center font-medium text-lg text-[#3A3A3A]' >Start your journey with us</p>
           <form className='flex flex-col mt-7 gap-2'>
             <input
               type="text"
               name="username"
               placeholder="Username"
+              // value={username}
+              // onChange={handleChange}
+              className='text-lg py-4 px-5 rounded-full outline-none'
+            />
+            <input
+              type="email"
+              name="email"
+              placeholder="Email"
               // value={username}
               // onChange={handleChange}
               className='text-lg py-4 px-5 rounded-full outline-none'
@@ -41,10 +33,10 @@ const Login = () => {
               // onChange={handleChange}
               className='text-lg py-4 px-5 rounded-full outline-none'
             />
-            <button type="submit" className='mt-2 text-center min-w-full bg-[#F4F6F9] text-black py-4 rounded-full hover:-translate-y-1 transition-all duration-300 hover:shadow-lg'>Log In</button>
+            <button type="submit" className='mt-2 text-center min-w-full bg-[#F4F6F9] text-black py-4 rounded-full hover:-translate-y-1 transition-all duration-300 hover:shadow-lg'>Sign up</button>
           </form>
           <Link to="/signup" className='text-end mt-1 text-sm'>
-            <p className='text-end mt-1 text-sm text-white hover:'>Don't have an Account?</p>
+            <p className='text-end mt-1 text-sm text-white hover:'>Already have an Account?</p>
           </Link>
         </div>
       </div>
@@ -53,7 +45,7 @@ const Login = () => {
       </div>
     </div>
   </div>
-  );
-};
+  )
+}
 
-export default Login;
+export default Signup
