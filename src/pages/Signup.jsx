b021349876c17d093703f7eb4506da33abdd6bdc
aryagami27 <94152700+aryagami27@users.noreply.github.com>
2024-10-12 13:08:07 +0530

    added google

--- src/pages/Signup.jsx
+++ src/pages/Signup.jsx
@@ -38,6 +38,7 @@ function Signup() {
           <Link to="/signup" className='text-end mt-1 text-sm'>
             <p className='text-end mt-1 text-sm text-white hover:'>Already have an Account?</p>
           </Link>
+          <button className='mt-2 text-center min-w-full bg-[#F4F6F9] text-black py-4 rounded-full hover:-translate-y-1 transition-all duration-300 hover:shadow-lg' >Signup using Google</button>
         </div>
       </div>
       <div className="w-[40%] h-full bg-[#FFE8E8] rounded-r-[40px]">
