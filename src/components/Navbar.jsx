96dca6720d09cbb86302eae32c4d81d8efa53f7c
aryagami27 <94152700+aryagami27@users.noreply.github.com>
2024-10-12 13:28:38 +0530

    Navbar Done

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -2,8 +2,17 @@ import React from 'react'
 
 function Navbar() {
   return (
-    <div className='w-screen h-16 bg-red-500'>
-      
+    <div className='w-screen h-16 shadow-lg flex items-center px-4 justify-between'>
+      <img src="./Logo.png" alt="" className='h-12' />
+      <div className='flex items-center gap-6 text-lg' >
+        <div className="">Wishlist</div>
+        <div className="">Travel</div>
+        <div className="">Memories</div>
+        <div className="">Journal</div>
+        <div className="size-10 rounded-full bg-[#E5F9F0] border">
+            <img src="" alt="" className='size-full '/>
+        </div>
+      </div>
     </div>
   )
 }
