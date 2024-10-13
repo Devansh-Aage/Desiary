d093c7e86865d720bca935f74914f6af80a8e97a
aryagami27 <94152700+aryagami27@users.noreply.github.com>
2024-10-13 18:17:30 +0530

    Assets updated home

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -8,10 +8,10 @@ const Home = () => {
         <p className="text-5xl font-alexBrush text-center mt-2" >A place to store your Dreams and wishes </p>
       </div>
       <div className="flex flex-wrap gap-4 p-8 justify-center">
-        <Card title="Wishlist" image='' path="/home/user/wishlist"/>
-        <Card title="Travel Bucket List" image='' path="/home/user/travel"/>
-        <Card title="Memories" image='' path="/home/user/memories"/>
-        <Card title="Journal" image='' path="/home/user/journal"/>
+        <Card title="Wishlist" image='/wishlist.jpg' path="/home/user/wishlist"/>
+        <Card title="Travel Bucket List" image='/travel.jpg' path="/home/user/travel"/>
+        <Card title="Memories" image='/memories.jpg' path="/home/user/memories"/>
+        <Card title="Journal" image='/journal.jpg' path="/home/user/journal"/>
       </div>
       
     </div>
@@ -22,8 +22,9 @@ const Card = ({title, image,path}) => {
   const navigate = useNavigate();
   return (
     <div className="bg-[#FF8B9C] cursor-pointer aspect-square w-80 h-80 rounded-lg relative" onClick={()=>navigate(path)} >
-      <img src={image} alt={title} />
-      <p className="text-2xl font-prata absolute bottom-3 text-center w-full">{title}</p>
+      <img src={image} alt={title} className="object-cover w-full h-full  rounded-lg" />
+      <div className="absolute h-full w-full bg-gradient-to-t from-white to-transparent top-0 to-30%"></div>
+      <p className="text-2xl font-prata text-center absolute bottom-3 w-full">{title}</p>
     </div>
   )
 }
