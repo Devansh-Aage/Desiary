f5bf809e9b5bdc4f017bb3c43b79b54e797c159e
aryagami27 <94152700+aryagami27@users.noreply.github.com>
2024-10-13 18:22:19 +0530

    Updated index and created journal

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -23,7 +23,7 @@ const Card = ({title, image,path}) => {
   return (
     <div className="bg-[#FF8B9C] cursor-pointer aspect-square w-80 h-80 rounded-lg relative" onClick={()=>navigate(path)} >
       <img src={image} alt={title} className="object-cover w-full h-full  rounded-lg" />
-      <div className="absolute h-full w-full bg-gradient-to-t from-white to-transparent top-0 to-30%"></div>
+      <div className="absolute h-full w-full bg-gradient-to-t from-white to-transparent top-0 to-35%"></div>
       <p className="text-2xl font-prata text-center absolute bottom-3 w-full">{title}</p>
     </div>
   )
--- src/pages/Journal.jsx
+++ src/pages/Journal.jsx
@@ -2,8 +2,20 @@ import React from 'react'
 
 function Journal() {
   return (
-    <div>
-      
+    <div className='w-full px-8 flex flex-wrap gap-6 mt-8 justify-center'>
+      <JournalCard title="Dreams" color="bg-[#FF8B9C]"/>
+      <JournalCard title="Wishes" color="bg-[#FFD180]"/>
+      <JournalCard title="Goals" color="bg-[#80FF91]"/>
+      <JournalCard title="Travel Bucket List" color="bg-[#809CFF]"/>
+      <JournalCard title="Memories" color="bg-[#FF8B9C]"/>
+    </div>
+  )
+}
+
+function JournalCard({title,color}){
+  return (
+    <div className={`w-80 h-80 rounded-lg ${color} flex items-center justify-center cursor-pointer`}>
+      <p className='text-2xl font-prata'>{title}</p>
     </div>
   )
 }
